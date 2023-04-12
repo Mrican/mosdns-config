072910dd659276a7006c5495c36305b06b1f315f
Mrican <73725611+Mrican@users.noreply.github.com>
2023-04-12 08:33:33 +0800

    Add files via upload

---
+++ service-restart.bat
@@ -0,0 +1,36 @@
+@set @_cmd=1 /*
+@echo off
+setlocal EnableExtensions
+title mosdns
+
+whoami /groups | findstr "S-1-16-12288" >nul && goto :admin
+if "%~1"=="RunAsAdmin" goto :error
+
+echo Requesting privileges elevation for managing the mosdns service . . .
+cscript /nologo /e:javascript "%~f0" || goto :error
+exit /b
+
+:error
+echo.
+echo Error: Administrator privileges elevation failed,
+echo        please manually run this script as administrator.
+echo.
+goto :end
+
+:admin
+pushd "%~dp0"
+mosdns service stop
+ipconfig /flushdns
+mosdns service start
+popd
+echo.
+echo Thank you for using mosdns!
+
+:end
+set /p =Press [Enter] to exit . . .
+exit /b */
+
+// JScript, restart batch script as administrator
+var objShell = WScript.CreateObject('Shell.Application');
+var ComSpec = WScript.CreateObject('WScript.Shell').ExpandEnvironmentStrings('%ComSpec%');
+objShell.ShellExecute(ComSpec, '/c ""' + WScript.ScriptFullName + '" RunAsAdmin"', '', 'runas', 1);
